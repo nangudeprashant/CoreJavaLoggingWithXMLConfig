ac92db376559db3401cad468528368bb47a58f8f
abc <abc@xyz.com>
2022-07-25 15:25:51 +0530

    Project guide file is added.

--- CoreJavaLoggingWithXMLConfig/src/main/java/com/itaspirants1/OtherApp.java
+++ CoreJavaLoggingWithXMLConfig/src/main/java/com/itaspirants1/OtherApp.java
@@ -1,11 +1,10 @@
-package com.itaspirants;
+package com.itaspirants1;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-public class App 
-{
-	private static final Logger logger = LogManager.getLogger(App.class);
+public class OtherApp {
+	private static final Logger logger = LogManager.getLogger(OtherApp.class);
 
 	public static void main(final String... args) 
 	{
@@ -13,5 +12,6 @@ public class App
 		logger.debug("Debug Message Logged !!!");
 		logger.info("Info Message Logged !!!");
 		logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
+		System.out.println("Now please refer file logs/appsLog.log for output of file appender which is specific to package of this file.");
 	}
 }
